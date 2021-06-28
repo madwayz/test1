03c63f9213245e98eb54fdc2f21a25e788725d94
madwayz <djurball@list.ru>
2021-06-29 02:48:22 +0700

    * fix view

--- web/apps/books/views.py
+++ web/apps/books/views.py
@@ -1,4 +1,4 @@
-from fastapi import Body, Request, Query, APIRouter
+from fastapi import APIRouter
 from base.provider import Database
 router = APIRouter()
 
@@ -6,4 +6,4 @@ router = APIRouter()
 @router.get("/writers/{writer_id}")
 def get_writer(writer_id: int):
     db = Database()
-    return db.get_books(writer_id)
+    return db.get_books(writer_id).get('json_build_object')
